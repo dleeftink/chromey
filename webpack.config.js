f83b24ff1791605882d865d931ae9dc24f4a36a9
dleeftink <dennisleeftink@gmail.com>
2023-03-13 14:43:48 +1000

    2 files modified

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     //globalObject: 'this',
     library: {
       name: 'chromey',
-      type: "commonjs"
+      type: "var"
     },
     libraryExport: 'default',
     module: true
